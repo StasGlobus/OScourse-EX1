19942c53acabb748d9ed7eaaaeed4c16f80fa32c
Stas Globus <stani246@gmail.com>
2019-11-11 22:57:43 +0200

    commit test 2

---
+++ Father/Program.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
+		return 1;
+	}
+	if (argc > 2)
+	{
+		return 1;
+	}
+	else
+	int a=0;
+	a = 1 + 2;
+
+
+
+
+
+
+	return 0;
+}
