3db1d485c55e1cbbfdb7f87b570f4e449a6b277e
Stas Globus <stani246@gmail.com>
2019-11-13 16:11:26 +0200

    changed strctual issues, cant have two files with the name Main as istructed, need to check that

---
+++ Son/main1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+
+
+int main()//int argc, char *argv[])
+{
+	int a = 0;
+	char test1[32] = { "1+3" }, test2[32] = { "22*5" };
+	printf("this is a %d\n", a);
+	/*
+	if (argc < 2)
+	{
+		return 1;
+	}
+	if (argc > 2)
+	{
+		return 1;
+	}
+	else
+	*/
+	a = 1 + 2;
+	printf("this is a %d\n", a);
+	getchar();
+
+
+
+
+
+
+	return 0;
+}
