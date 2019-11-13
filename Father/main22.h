8bceb9cd2800602723eb2f90c04fc55fb7fb66fc
Stas Globus <stani246@gmail.com>
2019-11-13 20:43:15 +0200

    added Basic Process handling to father

--- Father/main22.h
+++ Father/main22.h
@@ -7,4 +7,10 @@
 #include <process.h>
 #include <windows.h>
 
+#define TIMEOUT_IN_MILLISECONDS 5000
+#define BRUTAL_TERMINATION_CODE 0x55
+
+BOOL CreateProcessSimple(LPTSTR CommandLine, PROCESS_INFORMATION *ProcessInfoPtr);
+//void CreateProcessSimpleMain(void);
+
 #endif // MAIN22
